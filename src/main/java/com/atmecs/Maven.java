cbb9424a6b93a728df924400b0f053657c35c198
SaiAnusha1124 <53328089+SaiAnusha1124@users.noreply.github.com>
2019-08-13 19:12:03 +0530

    senddata

---
+++ src/main/java/com/atmecs/Maven.java
@@ -0,0 +1,16 @@
+package com.atmecs;
+
+import atmecs.List;
+
+public class Maven 
+{
+	public static void main(String[] args)
+	{
+		List l=new List();
+		l.Factorial();
+		l.Fibonacci();
+		l.Firstprimenumber();
+		
+	}
+
+}
